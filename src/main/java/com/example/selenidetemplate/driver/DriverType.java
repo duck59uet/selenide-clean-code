848b4e85dca926d30dab9d5406dff61de2ab8819
ducnv1911@gmail.com <ducnv1911@gmail.com>
2023-03-02 15:01:22 +0700

    initial code

---
+++ src/main/java/com/example/selenidetemplate/driver/DriverType.java
@@ -0,0 +1,66 @@
+package com.example.selenidetemplate.driver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+
+import java.io.File;
+import java.util.HashMap;
+
+public enum DriverType implements DriverSetup {
+
+    FIREFOX {
+        public RemoteWebDriver getWebDriverObject(DesiredCapabilities capabilities) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.merge(capabilities);
+            options.setHeadless(HEADLESS);
+
+            return new FirefoxDriver(options);
+        }
+    },
+    CHROME {
+        public RemoteWebDriver getWebDriverObject(DesiredCapabilities capabilities) {
+            HashMap<String, Object> chromePreferences = new HashMap<>();
+            chromePreferences.put("profile.password_manager_enabled", false);
+
+            ChromeOptions options = new ChromeOptions();
+            options.merge(capabilities);
+            options.setHeadless(HEADLESS);
+            options.addExtensions(new File("src/main/resources/extension_0_11_40_0.crx"));
+            options.addArguments("--no-default-browser-check");
+            options.setExperimentalOption("prefs", chromePreferences);
+
+            return new ChromeDriver(options);
+        }
+    },
+    EDGE {
+        public RemoteWebDriver getWebDriverObject(DesiredCapabilities capabilities) {
+            EdgeOptions options = new EdgeOptions();
+            options.merge(capabilities);
+
+            return new EdgeDriver(options);
+        }
+    },
+    SAFARI {
+        public RemoteWebDriver getWebDriverObject(DesiredCapabilities capabilities) {
+            SafariOptions options = new SafariOptions();
+            options.merge(capabilities);
+
+            return new SafariDriver(options);
+        }
+    };
+
+    public final static boolean HEADLESS = Boolean.getBoolean("headless");
+
+    @Override
+    public String toString() {
+        return super.toString().toLowerCase();
+    }
+}
