45da9cff1d89c797ed578bf8fefb33bf3aad0067
ducnv1911@gmail.com <ducnv1911@gmail.com>
2023-03-06 16:43:49 +0700

    fix(): add attribute POM.xml

--- src/main/java/com/example/selenidetemplate/config/Configuration.java
+++ src/main/java/com/example/selenidetemplate/config/Configuration.java
@@ -1,28 +1,28 @@
-//package com.example.selenidetemplate.config;
-//
-//import org.aeonbits.owner.Config;
-//import org.aeonbits.owner.Config.LoadPolicy;
-//import org.aeonbits.owner.Config.LoadType;
-//@LoadPolicy(LoadType.MERGE)
-//@Config.Sources({
-//        "system:properties",
-//        "classpath:general.properties",
-//        "classpath:local.properties",
-//        "classpath:selenium-grid.properties"})
-//public interface Configuration extends Config {
-//
-//    @Key("target")
-//    String target();
-//
-//    @Key("browser")
-//    String browser();
-//
-//    @Key("headless")
-//    Boolean headless();
-//
-//    @Key("url.base")
-//    String url();
-//
-//    @Key("timeout")
-//    int timeout();
-//}
+package com.example.selenidetemplate.config;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.LoadPolicy;
+import org.aeonbits.owner.Config.LoadType;
+@LoadPolicy(LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:general.properties",
+        "classpath:local.properties",
+        "classpath:selenium-grid.properties"})
+public interface Configuration extends Config {
+
+    @Key("target")
+    String target();
+
+    @Key("browser")
+    String browser();
+
+    @Key("headless")
+    Boolean headless();
+
+    @Key("url.base")
+    String url();
+
+    @Key("timeout")
+    int timeout();
+}
--- src/main/java/com/example/selenidetemplate/config/ConfigurationManager.java
+++ src/main/java/com/example/selenidetemplate/config/ConfigurationManager.java
@@ -1,13 +1,15 @@
-//package com.example.selenidetemplate.config;
-//
-//import org.aeonbits.owner.ConfigCache;
-//
-//public class ConfigurationManager {
-//
-//    private ConfigurationManager() {
-//    }
-//
-//    public static Configuration configuration() {
-//        return ConfigCache.getOrCreate(Configuration.class);
-//    }
-//}
+package com.example.selenidetemplate.config;
+
+import org.aeonbits.owner.ConfigCache;
+
+public class ConfigurationManager {
+
+    private ConfigurationManager() {
+    }
+
+    public static Configuration configuration() {
+        return ConfigCache.getOrCreate(Configuration.class);
+    }
+
+    public void
+}
